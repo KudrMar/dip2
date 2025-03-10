be001c3014840d3aa24b6f71f2d441ddf9efd4ef
Maria Kudryashova <kudrmar@list.ru>
2025-03-10 03:30:26 +0300

    250310

---
+++ src/components/Redux/store.jsx
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cities from "./cities";
+import routes from "./routes";
+import filter from "./filter";
+import seats from "./seats";
+import passengers from "./passengers";
+import order from "./order";
+
+export default  configureStore({
+  reducer: {
+    cities: cities,
+    routes: routes,
+    filter: filter,
+    seats: seats,
+    passengers: passengers,
+    order: order,
+  },
+});
